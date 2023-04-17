f7f2faaa5106ec8b39362585b0c17b484dbb504a
RJC <rauljarodconanan@gmail.com>
2023-04-17 10:50:36 +0800

    added basic gf chatbot

---
+++ chatbot/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from .views import chat
+
+urlpatterns = [
+    path('chatbot/', chat, name='chat')
+]
--- hackfest_ai_gf/settings.py
+++ hackfest_ai_gf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+5f2^otp*f7ojevkurm(x@vybb3_f^ru!1j3k&5d)1&6%zv*p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chatbot',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +84,8 @@ DATABASES = {
     }
 }
 
+# OPENAI
+HACKFEST_OPENAI_SECRET_KEY = config('HACKFEST_OPENAI_SECRET_KEY')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
